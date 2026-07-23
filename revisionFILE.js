2e9d87fa129d9e31a3e597aa36d0d25b4e7d902b
Lohan000 <lohannunes07@gmail.com>
2026-07-23 19:08:24 -0300

    Add files via upload

---
+++ revisionFILE.js
@@ -0,0 +1,79 @@
+const ask = require('readline-sync');
+
+
+//npm init -y
+//npm install readline-sync
+
+// 1 
+// let question = ask.question('Qual é o seu nome?');
+// let question1 = Number(ask.question('Qual é a sua idade?'));
+
+// console.log(`Olá ${question}, você tem ${question1} anos.`);
+
+// 2
+
+//let firstnumber1 = Number(ask.question('primerio Número'))
+//let secondNumber = Number(ask.question('Segundo Número'))
+
+//console.log(`${firstnumber1} + ${secondNumber} = ${firstnumber1 + secondNumber}`)
+//console.log(`${firstnumber1} * ${secondNumber} = ${firstnumber1 * secondNumber}`)
+//console.log(`${firstnumber1} - ${secondNumber} = ${firstnumber1 - secondNumber}`)
+//console.log(`${firstnumber1} / ${secondNumber} = ${firstnumber1 / secondNumber}`)
+
+// 3
+
+//let number1 = Number(ask.question('Primeira Número'))
+//let resultado = number1 >= 7 ? 'Aprovado':'Reprovado';
+
+//console.log(`${resultado}`)
+
+// 4
+
+// let number1 = Number(ask.question('Primeiro Número: '))
+// if (number1 %  2 == 0) {
+//         console.log('É par');
+// } else {
+//         console.log('é impar');
+// }
+
+// 5
+
+// let result = Number(ask.question('me da o dia da semana que se quer 1-7: '))
+
+// switch (result){
+//     case 1:
+//         console.log('Domingo')
+//         break
+//     case 2:
+//         console.log('Segunda')
+//         break
+//     case 3:
+//         console.log('Terca')
+//         break
+//     case 4:
+//         console.log('Quarta')
+//         break
+//     case 5:
+//         console.log('Quinta')
+//         break
+//     case 6:
+//         console.log('Sexta')
+//         break
+//     case 7:
+//         console.log('Sabado')
+//         break
+//     default:
+//         console.log('Erro')
+//         break
+// }
+
+// 7
+// function calculateSum(numbers){
+//     let soma = 0
+//     for(let i = 0; i < numbers.length; i++)
+//         soma = soma + numbers[i]
+//     console.log(soma)
+// }
+
+// let myArray = [7, 6, 4, 22, 13]
+// calculateSum(myArray)
