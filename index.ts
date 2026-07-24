039bd5fea244592ade3e598c3f4f0d018667db39
Lohan000 <lohannunes07@gmail.com>
2026-07-23 21:49:38 -0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,9 @@
+// let nome: string = "Daniel";
+// console.log("Olá, " + nome);
+const ask = require('readline-sync');
+
+function vezes(a: number): number {
+    return a * 2
+}
+
+let nome: number = ask.question('')
