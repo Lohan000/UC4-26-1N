05e9b374182606717d1b121fbf668698a0bab5f4
Lohan000 <lohannunes07@gmail.com>
2026-08-14 21:25:04 -0300

    Add files via upload

---
+++ Trabalho01/Character.ts
@@ -0,0 +1,54 @@
+export class Character{
+    protected name: string
+    protected level: number
+    protected health: number
+
+    public constructor(name: string, level: number, health: number){
+        this.name = name
+        this.level = level
+        this.health = health
+    }
+
+    public getterName(): string{
+        return this.name
+    }
+
+    public getterLevel(): number{
+        return this.level
+    }
+
+    public getterHealth(): number{
+        return this.health
+    }
+
+    public setterName(newName: string): void{
+        this.name = newName
+    }
+
+    public setterHealth(newhealth: number): void{
+        this.health = newhealth
+    }
+
+    public setterLevel(newLevel: number): void{
+        this.level = newLevel
+    }
+
+    public showInfo(): void{
+        console.log(`
+-------------------
+Character:
+-------------------
+Name: ${this.name}
+HP: ${this.health}
+LEVEL: ${this.level}`)
+    }
+
+    public TakeDamage(amount: number): void{
+        if(this.health > amount){
+            this.health -= amount
+        } else {
+            this.health = 0
+            console.log('Seu Personagem morreu')
+        }
+    }
+}
